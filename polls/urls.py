db8b263961cff9f83f04891d9387dd9081706d87
Filip Rybka <filip.rybka@venturedevs.com>
2019-05-20 14:18:51 +0200

    next tutorial's steps adding

--- polls/urls.py
+++ polls/urls.py
@@ -2,16 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-]
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
@@ -19,4 +11,6 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    # added the world 'specifics'
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -9,15 +11,20 @@ def index(request):
     context = {'latest_question_list' : latest_question_list}
     return render(request, 'polls/index.html', context)
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
