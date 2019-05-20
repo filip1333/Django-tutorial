c9624204545386ea71a176b8db8bb3f0ff8dff9d
Filip Rybka <filip.rybka@venturedevs.com>
2019-05-20 15:16:43 +0200

    generic view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,29 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+
+def get_queryset(self):
+    return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
 
 from .models import Choice, Question
 
