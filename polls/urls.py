ef4330d43e678ebd0de2f23dfdbcde7c36ab892d
Filip Rybka <filip.rybka@venturedevs.com>
2019-05-21 15:57:52 +0200

    edit admin

--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,11 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
     # added the world 'specifics'
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('specifics/<int:pk>/', views.detail, name='detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,37 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
 
-def index(request):
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+
+def get_queryset(self):
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
+
+def index1(request):
     latest_question_list = Question.objects.order_by('_pub_date')[:5]
-    context = {'latest_question_list' : latest_question_list}
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 
@@ -16,7 +40,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, question_id=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
@@ -25,7 +49,7 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 
-def result(request, question_id):
+def result1(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/result.html', {'question': question})
 
